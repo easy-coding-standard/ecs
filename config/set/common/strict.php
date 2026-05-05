3f5b2bd9faf49c218c547293594f1a7a90679a8e
Marcus Müller <25648755+M-arcus@users.noreply.github.com>
2026-05-05 10:31:07 +0200

    Update deprecation message for strict set

--- config/set/common/strict.php
+++ config/set/common/strict.php
@@ -4,4 +4,4 @@ declare (strict_types=1);
 namespace ECSPrefix202605;
 
 use Symplify\EasyCodingStandard\Exception\DeprecatedException;
-throw new DeprecatedException('The "strict" set is deprecated as it is dangerous to run without proper context. Please use the Rector rule "DeclareStrictTypesRector" instead.');
+throw new DeprecatedException('The "strict" set is deprecated as it is dangerous to run without proper context. Please use the Rector rule "SafeDeclareStrictTypesRector" instead.');
